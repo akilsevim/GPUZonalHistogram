a629c9395a62aea5c505f129b4d3dfceec291990
Akil Sevim <akilsevim@gmail.com>
2019-02-21 20:44:02 -0800

    support.h and support.cu added

--- Main.cu
+++ Main.cu
@@ -2,40 +2,69 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include "support.h"
+
 int main(int argc, char* argv[])
 {
+    Timer timer;
+    startTime(&timer);
+
     int c;
-    int lineCount = 0;
+    int line_count = 0;
     FILE *file;
-    file = fopen("cemetery.csv", "r");
+    cudaError_t cuda_ret;
+
+    printf("\nLoading file...");
+
+    char* filename = atoi(argv[1]);
+
+    file = fopen(filename, "r");
     if (file) {
         while ((c = getc(file)) != EOF)
-            if(c == '\n') lineCount++;
+            if(c == '\n') line_count++;
     }
-    printf("%d points loaded \n", lineCount);
 
-    float *lats = (float*) malloc(sizeof(float)*lineCount);
-    float *lons = (float*) malloc(sizeof(float)*lineCount);
+    float *lats_h = (float*) malloc(sizeof(float)*line_count);
+    float *lons_h = (float*) malloc(sizeof(float)*line_count);
 
     int i = 0;
     rewind(file);
-    while (i < lineCount) {
+    while (i < line_count) {
         fscanf(file, "%f,", &lats[i]);
         fscanf(file, "%f", &lons[i]);
         i++;
     }
 
-    i = 0;
-    while (i < lineCount) {
-        printf("lat:%f, lon:%f",lats[i], lons[i]);
-        i++;
-    }
-
-
-        
     fclose(file);
 
+    printf("\n%d points loaded. ", line_count);
+    stopTime(&timer); printf("%f s", elapsedTime(timer));
 
+    printf("Allocating device variables..."); fflush(stdout);
+    startTime(&timer);
+
+    cuda_ret = cudaMalloc((void**)&lats_d, line_count * sizeof(unsigned float));
+    if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
+    cuda_ret = cudaMalloc((void**)&lons_d, line_count * sizeof(unsigned float));
+    if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory");
+
+    cudaDeviceSynchronize();
+    
+    printf("Copying data from host to device..."); fflush(stdout);
+    startTime(&timer);
+
+    cuda_ret = cudaMemcpy(lats_d, lats_h, line_count * sizeof(unsigned float),
+        cudaMemcpyHostToDevice);
+    if(cuda_ret != cudaSuccess) FATAL("Unable to copy memory to the device");
+
+    cuda_ret = cudaMemset(lons_d, lons_h, line_count * sizeof(unsigned float));
+    if(cuda_ret != cudaSuccess) FATAL("Unable to set device memory");
+
+    cudaDeviceSynchronize();
+    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
+
+    cudaFree(lats_d); cudaFree(lons_d);
+    free(lats_h); free(lons_h);
 
     return 0;
 }
--- Makefile
+++ Makefile
@@ -7,9 +7,11 @@ OBJ	        = main.o
 
 default: $(EXE)
 
-main.o: main.cu
+main.o: main.cu support.h
 	$(NVCC) -c -o $@ main.cu $(NVCC_FLAGS)
 
+support.o: support.cu support.h
+	$(NVCC) -c -o $@ support.cu $(NVCC_FLAGS)
 $(EXE): $(OBJ)
 	$(NVCC) $(OBJ) -o $(EXE) $(LD_FLAGS)
 
