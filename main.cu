843d302bd34bf1c5d1abfcb0ff2ff88884a1aa84
Akil Sevim <akilsevim@gmail.com>
2019-03-20 22:28:29 -0700

    code clean

--- main.cu
+++ main.cu
@@ -19,7 +19,7 @@ int main(int argc, char* argv[])
     int stream_number = 3;
     //limit of the rows send to device
     int chunk_size = 25000;
-    int line_limit = 10000000;
+    int line_limit = 1000000000;
     FILE *file;
     cudaError_t cuda_ret;
 
@@ -33,7 +33,6 @@ int main(int argc, char* argv[])
     } else if(argc == 2) {
         filename = argv[1];
     } else if(argc == 3) {
-        
         if(strcmp(argv[2], "d") == 0) {
             filename = argv[1];
             debug = 1;
@@ -63,26 +62,12 @@ int main(int argc, char* argv[])
         if(cuda_ret != cudaSuccess) FATAL("Stream %d couldn't be created:%d",i, cuda_ret);
     }
 
-
-    /*fseek(file, 0L, SEEK_END); 
-  
-    // calculating the size of the file 
-    long int res = ftell(file);
-    printf("\nFile Size:%d", res);
-
-
-    rewind(file);
-    if (file) {
-        while ((c = getc(file)) != EOF)
-            if(c == '\n') line_count++;
-    }*/
-
-    
     point* points[stream_number];
     point* points_d[stream_number];
     unsigned int *histogram[stream_number];
     unsigned int *histogram_d[stream_number];
     for(int i = 0; i < stream_number; i++) {
+        //Unified Memory Implementation
         /*cuda_ret = cudaMallocManaged((void **) &points[i], sizeof(point)*chunk_size);
         if(cuda_ret != cudaSuccess) FATAL("Chunck %d couldn't be allocated:%d",i, cuda_ret);
         cuda_ret = cudaStreamAttachMemAsync(stream[i], points[i]);
@@ -106,14 +91,10 @@ int main(int argc, char* argv[])
         cudaDeviceSynchronize();
     }
 
-
-    
+    /**********Unified Memory Related Codes**********/
     /*cuda_ret = cudaMallocManaged((void **) &histogram, sizeof(unsigned int)*histo_size);
     if(cuda_ret != cudaSuccess) FATAL("Histogram couldn't be allocated on device: %d", cuda_ret);*/
-
-    
     //memset(histogram, 0, histo_size * sizeof(unsigned int));
-    
     //float *lats_d;
     //float *lons_d;
     //unsigned int *histo_d;
@@ -131,8 +112,9 @@ int main(int argc, char* argv[])
     if(file == NULL) {
         perror("fopen()");
     }
-    int test_1 = 0;
     
+    //Read and print first 20 lines for deubgging purposes
+    int test_1 = 0;
     if(debug == 1) {
         char * line = NULL;
         size_t len = 0;
@@ -143,43 +125,35 @@ int main(int argc, char* argv[])
             if(count == 20) break;
             count++;
         }
-        /*for(int i = 0; i < 10; i++) {
-            
-            //test_1 = fscanf(file, "%*s\t%f\t%f\t%*s\n", &lat, &lon);
-            //test_1 = fscanf(file, "%f,%f\n", &lat, &lon);
-            //printf("\nlat:%f, lon:%f", lat, lon);
-            char* s;
-            fscanf(file, "%s\n", s);
-            printf("%s\n",s);
-        }*/
         fclose(file);
         return 0;
     }
     
+    //Worl coordinate max and min values to calculate histogram
     float lat_max = 90.0f, lat_min = -90.0f, lon_max = 180.0f, lon_min = -180.0f;
     //lat_max:78.213684, lon_max:178.487671, lat_min:-54.901825, lon_min:-176.255707
 
-
-    
     char* line;
     int end = 0;
     int chunk_counter = 0;
     int test = 0;
-
     unsigned int *histogram_final;
     histogram_final = (unsigned int*)malloc(histo_size * sizeof(unsigned int));
     memset(histogram_final, 0.0f, histo_size * sizeof(unsigned int));
     
     while (1) {
-        //printf("\n%d",chunk_counter);
-        
         for(int i = 0; i < stream_number; i++) {
             float lat, lon = lat = 0.0f;
             
-            //test = fscanf(file, "%f,%f\n", &lon, &lat);
-            test = fscanf(file, "%*s\t%f\t%f\t%*s\n", &lat, &lon);
+            
+            if(strcmp(filename, "cemetery.csv") == 0)) {
+                //pattern for cemetery dataset
+                test = fscanf(file, "%f,%f\n", &lon, &lat);
+            } else {
+                //pattern for all_nodes dataset
+                test = fscanf(file, "%*s\t%f\t%f\t%*s\n", &lat, &lon);
+            }
 
-            //printf("\nlat:%f, lon:%f", lat, lon);
             if(test == EOF) {//If we reached EOF
                 end = 1;
                 break;
@@ -187,28 +161,25 @@ int main(int argc, char* argv[])
                 i--;
                 continue;
             }
-            /*if(lat > lat_max) lat_max = lat;
-            if(lon > lon_max) lon_max = lon;
-            if(lat < lat_min) lat_min = lat;
-            if(lon < lon_min) lon_min = lon;*/
+            //if there is an outlier set it to the limits, we could have skip those rows also but prefferd to add them to camputation
+            if(lat > lat_max) lat = lat_max;
+            if(lon > lon_max) lon = lon_max;
+            if(lat < lat_min) lat = lat_min;
+            if(lon < lon_min) lon = lon_min;
             points[i][chunk_counter].lat = lat;
             points[i][chunk_counter].lon = lon;
-            //printf("\nS==>Lat:%f, Lon:%f",points[i][chunk_counter].lat, points[i][chunk_counter].lon);
-
             line_count++;
-
-            if(line_limit == line_limit) {
+            
+            //if a limit set check it
+            if(line_limit != 0 && line_limit == line_count) {
                 end = 1;
                 break;
             }
         }
 
-        //printf("\nlat_max:%f, lon_max:%f, lat_min:%f, lon_min:%f", lat_max, lon_max, lat_min, lon_min);
-
         chunk_counter++;
 
-        if(chunk_counter == chunk_size || (end == 1 && chunk_counter != 1)) {
-
+        if(chunk_counter == chunk_size || (end == 1 && chunk_counter != 1) || line_limit == line_count) {
             if(lat_max > FLT_MAX) lat_max = FLT_MAX;
             if(lon_max > FLT_MAX) lon_max = FLT_MAX;
             if(lat_min > FLT_MIN) lat_min = FLT_MIN;
@@ -217,13 +188,9 @@ int main(int argc, char* argv[])
             for(int i = 0; i < stream_number; i++) {
                 sent_size = chunk_size;
                 if(line_count%chunk_size != 0) sent_size = line_count%chunk_size;
-                //printf("\nSENT SIZE:%d",sent_size);
                 cudaMemcpyAsync(points_d[i], points[i], sent_size*sizeof(point*), cudaMemcpyHostToDevice, stream[i]);
-                    
             }
             for(int i = 0; i < stream_number; i++) {
-                //printf("\nKernel Call:%d, LineCounter:%d, Chunk Counter:%d",i,line_count, chunk_counter);
-                //printf("\nlat_max:%f, lon_max:%f, lat_min:%f, lon_min:%f", lat_max, lon_max, lat_min, lon_min);
                 histogram_kernel<<<DimGrid, DimBlock, histo_size * sizeof(unsigned int), stream[i]>>>(
                     points_d[i],
                     histogram_d[i],
@@ -236,12 +203,8 @@ int main(int argc, char* argv[])
                     lon_min);
             }
 
-            
-
             for(int i = 0; i < stream_number; i++) {
-
                 cudaMemcpyAsync(histogram[i], histogram_d[i], histo_size*sizeof(unsigned int), cudaMemcpyDeviceToHost, stream[i]);
-                    
             }
 
             for(int i = 0; i < stream_number; i++) {
@@ -250,26 +213,15 @@ int main(int argc, char* argv[])
 
             for(int i = 0; i < stream_number; i++) {
                 for(int j = 0; j < histo_size; j++) {
-                    //printf("\nHisto[%d][%d]:%d",i,j, histogram[i][j]);
                     histogram_final[j] += histogram[i][j];
                 }
             }
-            
-            /*for(int i = 0; i < stream_number; i++) {
-                memset(&points[i]->lat, 0.0f, sizeof(float)*chunk_size);
-                memset(&points[i]->lon, 0.0f, sizeof(float)*chunk_size);
-            }*/
             
             chunk_counter = 0;
 
             for(int i = 0; i < stream_number; i++) {
                 cuda_ret = cudaMemset(histogram_d[i], 0, histo_size * sizeof(unsigned int));
                 if(cuda_ret != cudaSuccess) FATAL("Unable to set device histogram %d", i);
-                /*printf("Stream 1:");
-                for(int j = 0; j < chunk_size; j++) {
-                    printf("\nS==>Lat:%f, Lon:%f",points[i][j].lat, points[i][j].lon);
-                }*/
-                
             }
 
         }
@@ -279,29 +231,15 @@ int main(int argc, char* argv[])
         
     }
 
-    /*for(int k = 0; k < histo_col_count; k++) {
-        for(int l = 0; l < histo_row_count; l++) {
-            
-            printf("\n%d\t%d\t%d", k, l, histogram[0][k * histo_col_count + l]);
-        }
-    }*/
-    
-    //printf("lat_max:%f, lon_max:%f, lat_min:%f, lon_min:%f\n", lat_max, lon_max, lat_min, lon_min);
-
-    
-    
-    
-
-
     fclose(file);
 
     FILE *out_file;
     out_file = fopen(output_file, "w");
-
+    printf("Done!");
     for(int i = 0; i < histo_row_count; i++) {
         for(int j = 0; j < histo_col_count; j++) {
             fprintf(out_file, "%d\t%d\t%d\n", i, j, histogram_final[i * histo_col_count + j]);
-            //printf("\n%d\t%d\t%d", i, j, histogram_final[i * histo_col_count + j]);
+            printf("\n%d\t%d\t%d", i, j, histogram_final[i * histo_col_count + j]);
         }
     }
 
@@ -313,6 +251,5 @@ int main(int argc, char* argv[])
     for(int i = 0; i < stream_number; i++) {
         cudaStreamDestroy(stream[i]);
     }
-    
     return 0;
 }
